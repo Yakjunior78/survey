5515defe226cc4a236f5ba937f8a5e532ef138ca
Ya'kov Erick <yakov.erick@roamtech.com>
2020-10-13 10:50:45 +0300

    resolve bug

--- app/Modules/Surveys/Form.js
+++ app/Modules/Surveys/Form.js
@@ -1,24 +1,24 @@
 const { validate } = use('Validator');
 
 class SurveyForm {
-	
-	async validate(request) {
-		
-		const rules = {
-			title: 'required',
-			company_id: 'required'
-		}
-		
-		return await validate (request, rules);
-	}
-	
-	async error(validation) {
-		return {
-			status: 401,
-			data: validation.messages(),
-			message: 'Missing form fields'
-		}
-	}
+
+    async validate(request) {
+
+        const rules = {
+            title: 'required',
+            identity: 'required'
+        }
+
+        return await validate (request, rules);
+    }
+
+    async error(validation) {
+        return {
+            status: 401,
+            data: validation.messages(),
+            message: 'Missing form fields'
+        }
+    }
 }
 
 module.exports = SurveyForm;
