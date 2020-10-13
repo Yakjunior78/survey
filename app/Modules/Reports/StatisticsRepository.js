4e70cfe323419c70cb4b1563790350831fa117f5
Ya'kov Erick <yakov.erick@roamtech.com>
2020-10-13 11:41:51 +0300

    rank questions in order of ranks

--- app/Modules/Reports/StatisticsRepository.js
+++ app/Modules/Reports/StatisticsRepository.js
@@ -5,30 +5,30 @@ const SurveyModel = use('App/Models/Survey');
 const { transform } = use('App/Helpers/Transformer');
 
 class StatisticsRepository {
-	
+
 	async instanceQuestions(survey_id, instance_id)
 	{
 		let instance = await InstanceModel.query().where('uuid', instance_id).first();
-		
+
 		let survey = await SurveyModel.query().where('uuid', survey_id).first();
-		
-		let questions = instance ? await instance.questions().fetch() : await survey.questions().fetch();
-		
+			
+		let questions = instance ? await instance.questions().orderBy('rank', 'asc').fetch() : await survey.questions().fetch();
+
 		questions = questions.toJSON();
-		
+
 		let channel = instance ? await instance.channel().first() : {};
-		
+
 		let transformedQuestions = [];
-		
+
 		for (let i = 0; i < questions.length; i++) {
-			
+
 			let question = await QuestionModel.find(questions[i].id);
-			
+
 			let transformedQuestion =  await transform(question, 'QuestionStatistics', channel);
-			
+
 			await transformedQuestions.push(transformedQuestion);
 		}
-		
+
 		return transformedQuestions;
 	}
 }
