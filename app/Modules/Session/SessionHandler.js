bf9e64f86951fb13ef60d97af71ac5cb6b9c03f2
Yakov <yakjunior78@gmail.com>
2020-11-20 10:03:48 +0300

    testing contacts

--- app/Modules/Session/SessionHandler.js
+++ app/Modules/Session/SessionHandler.js
@@ -1,5 +1,8 @@
 const repo = new(use('App/Modules/Session/SessionRepository'))();
 const SessionTrailModel = use('App/Models/SessionTrail');
+const SessionModel = use('App/Models/Session');
+
+const { getStatus } = use('App/Helpers/Emalify');
 
 class SessionHandler {
 	
@@ -25,13 +28,23 @@ class SessionHandler {
 		
 		await latestTrail.save();
 		
-		if(!next) return null;
+		if(!next) return await this.deactivateSession(session);
 		
 		return await SessionTrailModel.create({
 			session_id: session.id,
 			question_id: next.id,
 			consent: false
 		});
+	}
+	
+	async deactivateSession(session)
+	{
+		let status = await getStatus('closed');
+		
+		return await SessionModel
+			.query()
+			.where('id', session.id)
+			.update({'status_id': status.id});
 	}
 }
 
