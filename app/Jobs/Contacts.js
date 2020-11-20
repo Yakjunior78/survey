c76cab5377761f2f939e0e4782fa64f83e3743f7
Yakov <yakjunior78@gmail.com>
2020-11-20 08:46:22 +0300

    testing contacts

--- app/Jobs/Contacts.js
+++ app/Jobs/Contacts.js
@@ -3,12 +3,45 @@ const Database = use('Database');
 class Contacts {
 	async sync(instance)
 	{
-		let group = await Database.connection('mysqlSMS')
-			.from('contact_groups')
-			.where('id', instance.group_id)
-			.first();
+		let group = await this.group(instance.group_id);
 		
-		console.log(group.id);
+		if(!group) {
+			return;
+		}
+		
+		let file = await this.file(group.id);
+		
+		if(!file) {
+			return;
+		}
+		
+		let contacts = await this.contacts(file.table_name);
+		
+		console.log(contacts);
+	}
+	
+	async group(id)
+	{
+		return Database.connection ('mysqlSMS')
+			.from ('contact_groups')
+			.where ('id', id)
+			.first ();
+	}
+	
+	async file(id)
+	{
+		return Database.connection ('mysqlSMS')
+			.from ('file_upload_queues')
+			.where ('contact_groups_id', id)
+			.first ();
+	}
+	
+	async contacts(table)
+	{
+		return await Database
+			.connection('mysqlContacts')
+			.from(table)
+			.all();
 	}
 }
 
