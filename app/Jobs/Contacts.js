af62a07d06f2c6f9d5debe8991ea6be073f3ffda
Yakov <yakjunior78@gmail.com>
2020-11-20 08:35:26 +0300

    testing contacts

--- app/Jobs/Contacts.js
+++ app/Jobs/Contacts.js
@@ -3,9 +3,11 @@ const Database = use('Database');
 class Contacts {
 	async sync(instance)
 	{
-		let groups = await Database.connection('mysqlSMS').table('contact_groups').select('*');
+		let group = await Database.connection('mysqlSMS')
+			.table('contact_groups')
+			.find(instance.group_id);
 		
-		console.log(groups);
+		console.log(group);
 	}
 }
 
