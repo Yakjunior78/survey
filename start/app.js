83b8d8a77ffee08559ee03c744d465c725581580
Yakov <yakjunior78@gmail.com>
2020-11-26 11:47:39 +0300

    refactor ace commands

--- start/app.js
+++ start/app.js
@@ -60,7 +60,8 @@ const aliases = {}
 |
 */
 const commands = [
-    
+    'App/Commands/Quote',
+    'App/Commands/CloneContactsConsumer'
 ]
 
 module.exports = { providers, aceProviders, aliases, commands }
