24c70d2010510df0c51225efadd10750a13d3598
Yakov <yakjunior78@gmail.com>
2020-11-26 19:29:23 +0300

    testing

--- app/Helpers/Question.js
+++ app/Helpers/Question.js
@@ -20,7 +20,7 @@ const smsReply = async (question) => {
 	
 	switch(type.slug) {
 		case 'multiple_choice':
-			let choices = question.options;
+			let choices = question.options().fetch();
 			console.log(choices, 'these are the choices');
 			choice_string = await formatChoices(choices);
 			break;
