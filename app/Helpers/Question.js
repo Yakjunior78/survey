59045b569f2e1f41d9c2fc5d8ded967e59e4ab95
Yakov <yakjunior78@gmail.com>
2020-11-27 15:06:21 +0300

    clean up response handler

--- app/Helpers/Question.js
+++ app/Helpers/Question.js
@@ -19,6 +19,7 @@ const smsReply = async (question) => {
 	switch(type.slug) {
 		case 'multiple_choice':
 			let choices = question.choices().fetch();
+			console.log(choices, 'these are the choices');
 			choice_string = await formatChoices(choices);
 			break;
 		case 'open_ended':
--- app/Services/SMS/Send.js
+++ app/Services/SMS/Send.js
@@ -14,8 +14,6 @@ class Send {
 		
 		let headers = await Auth.headers();
 		
-		console.log(data, 'this is the data');
-		
 		return await axios.post(base_url + '/projects/'+projectId+'/sms/bulk', data, { headers: headers })
 			.then( ({ data }) => {
 				console.log(data, 'sms sent successfully');
