7d04b7c7d5020513b8da0f30e0893f9edff7c0e2
Yakov <yakjunior78@gmail.com>
2020-12-11 16:29:53 +0300

    enable send sms survey

--- app/Services/Survey/SMS.js
+++ app/Services/Survey/SMS.js
@@ -9,16 +9,15 @@ class SMS {
 			return await this.clone(instance);
 		}
 		
+		if(!instance.session_job_queued) {
+			return await this.createSessions(instance);
+		}
+
+		if(!instance.send_sms_job_queued) {
+			return await this.dispatch(instance);
+		}
+
 		return instance;
-		// if(!instance.session_job_queued) {
-		// 	return await this.createSessions(instance);
-		// }
-		//
-		// if(!instance.send_sms_job_queued) {
-		// 	return await this.dispatch(instance);
-		// }
-		//
-		// return instance;
 	}
 	
 	async clone(instance)
