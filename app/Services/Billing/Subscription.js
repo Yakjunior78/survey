958cbdec4ea58a30cfd728a77a45607cedb56358
Yakov <yakjunior78@gmail.com>
2020-12-14 21:59:28 +0300

    testing reation of billing accounts

--- app/Services/Billing/Subscription.js
+++ app/Services/Billing/Subscription.js
@@ -55,7 +55,7 @@ class Subscription {
 		return {
 			"name": "Subscription for survey",
 			"plans": [
-				"61"
+				"60"
 			],
 			"customer_id": account.customer_id,
 			"description": "Default subscription for survey",
