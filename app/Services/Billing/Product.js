5e761c9b90ddda2080f7f0f27b059ab1efb89c88
Yakov <yakjunior78@gmail.com>
2020-12-15 00:19:10 +0300

    testing reation of billing accounts

--- app/Services/Billing/Product.js
+++ app/Services/Billing/Product.js
@@ -5,19 +5,28 @@ const moment = use('moment');
 class Product {
 	async store(user)
 	{
-		return Database
-			.connection ('mysqlAuth')
-			.table ('user_products')
-			.insert ({
-				user_id: user.id,
-				product_id: Env.get('SURVEY_PRODUCT_ID'),
-				customer_account: user.customer_account,
-				date_subscribed: moment(Date.now()).format('YYYY-MM-DD HH:mm:ss'),
-				active: 1,
-				setup: 1,
-				created_at: moment(Date.now()).format('YYYY-MM-DD HH:mm:ss'),
-				updated_at: moment(Date.now()).format('YYYY-MM-DD HH:mm:ss'),
-			});
+		let data = await this.data(user);
+		
+		console.log(data, 'this is the data');
+		
+		// return Database
+		// 	.connection ('mysqlAuth')
+		// 	.table ('user_products')
+		// 	.insert ();
+	}
+	
+	async data(user)
+	{
+		return {
+			user_id: user.id,
+			product_id: Env.get('SURVEY_PRODUCT_ID'),
+			customer_account: user.customer_account,
+			date_subscribed: moment(Date.now()).format('YYYY-MM-DD HH:mm:ss'),
+			active: 1,
+			setup: 1,
+			created_at: moment(Date.now()).format('YYYY-MM-DD HH:mm:ss'),
+			updated_at: moment(Date.now()).format('YYYY-MM-DD HH:mm:ss'),
+		}
 	}
 }
 
--- app/Services/Billing/Subscription.js
+++ app/Services/Billing/Subscription.js
@@ -54,7 +54,7 @@ class Subscription {
 		return {
 			"name": "Subscription for survey",
 			"plans": [
-				"32"
+				"31"
 			],
 			"customer_id": account.customer_id,
 			"description": "Default subscription for survey",
