99614e9321e6aadca9bede3fcea696931f072828
Yakov <yakjunior78@gmail.com>
2021-02-19 16:47:54 +0300

    modified date helpers

--- app/Helpers/DateHelper.js
+++ app/Helpers/DateHelper.js
@@ -10,6 +10,10 @@ const isNowOrPast = async (dateTime) => {
 	
 	let now = new Date();
 	
+	now.setHours( now.getHours() + 3 );
+	
+	console.log(dateTime, now, 'these are the dates');
+	
 	return !!(
 		moment (dateTime) < moment (now) ||
 		moment (dateTime).isSame (moment (now), 'time')
