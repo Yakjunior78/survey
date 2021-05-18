44be1b7384c0992793755beaa2a71d86caf5d493
Yakov <yakjunior78@gmail.com>
2021-05-18 08:04:07 +0300

    clean up

--- app/Controllers/Http/ChannelsController.js
+++ app/Controllers/Http/ChannelsController.js
@@ -4,16 +4,18 @@ const ChannelModel = use('App/Models/Channel');
 const InteractionModel = use('App/Models/Interaction');
 
 class ChannelsController {
-	
+
 	async index({ response })
 	{
 		let channels = await ChannelModel.all();
+
 		return response.json(channels);
 	}
-	
+
 	async interactionModes({ response })
 	{
 		let modes = await InteractionModel.all();
+		
 		return response.json(modes);
 	}
 }
