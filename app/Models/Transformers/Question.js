91bf1522d8e4b41a158d573687f76b02c7099892
Yakov <yakjunior78@gmail.com>
2021-05-29 08:24:13 +0300

    transform questions

--- app/Models/Transformers/Question.js
+++ app/Models/Transformers/Question.js
@@ -1,16 +1,16 @@
 const ConditionModel = use('App/Models/Condition');
 
 class QuestionTransformer {
-	
+
 	async transform(question) {
-		
+
 		if(!question) {
 			return {};
 		}
-		
+
 		let type = await question.type().first();
 		let inputType = await question.inputType().first();
-		
+
 		return {
 			id: question.id,
 			uuid: question.uuid,
@@ -23,22 +23,23 @@ class QuestionTransformer {
 			conditions: await this.conditions(question),
 			question_type_id: type ? type.id : null,
 			questionType: type ? type : null,
-			rank: question.rank
+			rank: question.rank,
+			response: await question.responses().fetch()
 		}
 	}
-	
+
 	async conditions(question)
 	{
 		let conditions = await question.conditions().orderBy('created_at', 'desc').fetch();
-		
+
 		conditions = conditions.toJSON();
-		
+
 		let transformedConditions = [];
-		
+
 		for (let i = 0; i < conditions.length; i++) {
-			
+
 			let condition = await ConditionModel.findOrFail(conditions[i].id);
-			
+
 			let transformed = {
 				id: condition.id,
 				question_id: condition.question_id,
@@ -50,10 +51,10 @@ class QuestionTransformer {
 				end: condition.end,
 				operand: await condition.operand().first()
 			}
-			
+
 			transformedConditions.push(transformed);
 		}
-		
+
 		return transformedConditions;
 	}
 }
--- app/Modules/Responses/ResponseRepository.js
+++ app/Modules/Responses/ResponseRepository.js
@@ -4,59 +4,73 @@ const InstanceModel = use('App/Models/Instance');
 const SurveyModel = use('App/Models/Survey');
 const ResponseModel = use('App/Models/Response');
 const SessionModel = use('App/Models/Session');
+const QuestionModel = use('App/Models/Question');
 
 const { transform } = use('App/Helpers/Transformer');
 
 const Database = use('Database');
 
 class ResponseRepository {
-	
+
 	async responses(data)
 	{
 		let instance = null;
 		let survey = null;
-		
+
 		if(data.instance_id) {
 			instance = await InstanceModel.findBy('uuid', data.instance_id);
 		}
-		
+
 		if(!instance) {
 			return [];
 		}
-		
+
 		survey = instance
 			?  await instance.survey().first()
 			: await SurveyModel.findBy('uuid', data.survey_id);
-		
+
 		let channel = instance ? await instance.channel().first() : null;
-		
+
 		let sessions = await SessionModel
 			.query()
 			.where('instance_id', instance.id)
 			.orderBy('updated_at', 'desc')
 			.paginate(data.page ? data.page : 1, 8);
-		
+
 		sessions = sessions ? sessions.toJSON() : [];
-		
+
 		let sessionList = sessions.data;
-		
+
 		let transformedSessions = [];
-		
+
 		for (let i = 0; i < sessionList.length; i++)
 		{
 			let session = await SessionModel.query().where('id', sessionList[i].id).first();
-			
+
 			let transformed = await transform (session, 'Session');
-			
+
 			transformedSessions.push(transformed);
 		}
-		
+
 		let questions = await instance.questions().orderBy('rank', 'asc').fetch();
-		
+
+		questions = questions ? questions.toJSON() : [];
+
+		let transformedQuestions = [];
+
+		for (let i = 0; i < questions.length; i++)
+		{
+			let q = await QuestionModel.query().where('id', questions[i].id).first();
+
+			let transformedQ = await transform (q, 'Question');
+
+			transformedQuestions.push(transformedQ);
+		}
+
 		sessions.data = transformedSessions;
-		
+
 		return {
-			questions: questions,
+			questions: transformedQuestions,
 			sessions: sessions
 		};
 	}
