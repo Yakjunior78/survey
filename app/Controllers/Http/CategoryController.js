99ed2cc4c123e3df429090c19b0f432a5cb56f24
Yakov <yakjunior78@gmail.com>
2021-05-30 19:59:12 +0300

    clean up

--- app/Controllers/Http/CategoryController.js
+++ app/Controllers/Http/CategoryController.js
@@ -3,6 +3,7 @@
 const CategoryRepo = new(use('App/Modules/Categories/CategoryRepository'))();
 
 class CategoryController {
+	
 	async index({ response }) {
 		return response.json({
 			categories: await CategoryRepo.get()
